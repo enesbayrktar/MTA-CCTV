58757cca7ff34d6a9c86624be3adf38d2bee8af8
Enes Bayraktar <steam.enesbayraktar@gmail.com>
2020-12-23 22:20:53 +0300

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,174 @@
+screenWidth, screenHeight = guiGetScreenSize()
+scale = screenHeight / 1080
+font = 'arial'
+isCCTV = false
+cctvPositions = {}
+currentCamX, currentCamY, currentCamZ, currentCamInt = 0, 0, 0, 0
+events = { 'cctv.positions.set', 'cctv.player.set' }
+
+-- **************************************************************************
+
+guiSetInputMode('no_binds_when_editing')
+
+for i, event in ipairs(events) do
+  addEvent(event, true)
+end
+
+addEventHandler('onClientResourceStart', resourceRoot, function()
+    return triggerServerEvent('cctv.positions.get', resourceRoot)
+  end
+)
+
+addEventHandler('cctv.positions.set', localPlayer, function(positions)
+    cctvPositions = positions
+    return triggerServerEvent('cctv.handle.cmd', resourceRoot)
+  end
+)
+
+-- **************************************************************************
+
+function dxCreateFramedText(text, left, top, right, bottom, color, scaleT, font, alignX, alignY, clip, wordBreak, postGUI, outline)
+	local self = {}
+	local rt = dxCreateRenderTarget(right, bottom, true)
+
+	self.draw = function()
+		dxDrawImage(left, top, right, bottom, rt)
+	end
+
+	self.update = function()
+		dxSetRenderTarget(rt, true)
+    for oX = (outline * -1), outline do
+      for oY = (outline * -1), outline do
+        dxDrawText (text, oX, oY, right + oX, bottom + oY, tocolor(0, 0, 0, 255), scaleT, font, alignX, alignY, clip, wordBreak, postGUI, colorCoded, subPixelPositioning, fRotation, fRotationCenterX, fRotationCenterY)
+      end
+    end
+    dxDrawText (text, 0, 0, right, bottom, color, scaleT, font, alignX, alignY, clip, wordBreak, postGUI, colorCoded, subPixelPositioning, fRotation, fRotationCenterX, fRotationCenterY)
+		dxSetRenderTarget()
+	end
+
+	self.setText = function(v)
+		text = v
+		self.update()
+	end
+
+	self.setColor = function(v)
+		color = v
+		self.update()
+	end
+
+	self.update()
+
+	return self
+end
+
+textW, textH = scale * 350, scale * 25
+textX = screenWidth - textW - scale * 15
+
+textY = textH + scale * 15
+camY = textY + scale * 15
+locationY = textY + scale * 30
+
+dxElements = {
+  dxCreateFramedText('CCTV', textX, textY, textW, textH, 0xFFFFFFFF, 1, 'default-bold', 'right', 'center', false, false, false, scale * 2),
+  dxCreateFramedText('KAMERA:', textX, camY, textW, textH, 0xFFFFFFFF, 1, font, 'right', 'center', false, false, false, scale * 2),
+  dxCreateFramedText('LOKASYON:', textX, locationY, textW, textH, 0xFFFFFFFF, 1, font, 'right', 'center', false, false, false, scale * 2),
+}
+
+-- **************************************************************************
+
+function render()
+  for i, element in ipairs(dxElements) do
+    element.draw()
+  end
+end
+
+updateLocationTimer = nil;
+
+function updateLocation()
+  dxElements[2].setText(string.format('KAMERA:  %s', currentCamInt))
+  dxElements[3].setText(string.format('LOKASYON:  %s', getZoneName(currentCamX, currentCamY, currentCamZ)))
+end
+
+-- **************************************************************************
+
+function nextCamera()
+  if not (currentCamInt + 1 > #cctvPositions) then
+    return setCCTV(currentCamInt + 1)
+  end
+end
+
+function prevCamera()
+  if not (currentCamInt - 1 < 1) then
+    return setCCTV(currentCamInt - 1)
+  end
+end
+
+function bind()
+  bindKey('arrow_l', 'down', prevCamera)
+  bindKey('arrow_r', 'down', nextCamera)
+end
+
+function unbind()
+  unbindKey('arrow_l', 'down')
+  unbindKey('arrow_r', 'down')
+end
+
+keys = {
+  [true] = bind,
+  [false] = unbind
+}
+
+function bindKeys(state)
+  return keys[state]()
+end
+
+-- **************************************************************************
+
+function setCCTV(cctvNumber)
+  local item = cctvPositions[cctvNumber]
+
+  currentCamX, currentCamY, currentCamZ = item[1], item[2], item[3];
+  currentCamInt = cctvNumber;
+
+  setCameraMatrix(item[1], item[2], item[3], item[4], item[5], item[6])
+end
+
+function openCCTV(cctvNumber)
+  setCCTV(cctvNumber)
+
+  if not isCCTV then
+    setElementFrozen(localPlayer, true)
+    addEventHandler('onClientRender', root, render)
+    updateLocationTimer = setTimer(updateLocation, 200, 0)
+    bindKeys(true)
+    isCCTV = true;
+  end
+end
+
+function closeCCTV(_)
+  removeEventHandler('onClientRender', root, render)
+  if isTimer(updateLocationTimer) then
+    killTimer(updateLocationTimer)
+  end
+
+  setElementFrozen(localPlayer, false)
+
+  currentCamInt = 0;
+  currentCamX, currentCamY, currentCamZ = 0, 0, 0;
+
+  setCameraTarget(localPlayer)
+  bindKeys(false)
+  isCCTV = false
+end
+
+-- **************************************************************************
+
+local actions = {
+  [true] = openCCTV,
+  [false] = closeCCTV
+}
+
+addEventHandler('cctv.player.set', localPlayer, function(state, cctvNumber)
+    return actions[state](cctvNumber)
+  end
+)
---
+++ server.lua
@@ -0,0 +1,89 @@
+loggedIn = 'player.logged' -- loggedIn data
+cctv = 'player.can.cctv' -- cctv data, if player has that he can be use /cctv
+debugmode = true -- debugmode, if true then everyone access to /cctv command
+events = { 'cctv.positions.get', 'cctv.handle.cmd' }
+positions = {
+  -- Positions taken from https://github.com/bekiroj/mtasa-resources/blob/main/mtasa-owl-cctv/main.lua
+  -- ex: { positionX, positionY, positionZ, lookAtX, lookAtY, lookAtZ, roll, fov }
+  { 1929.7922363281, -1738.6701660156, 23.292699813843, 1930.1812744141, -1739.5089111328, 22.911764144897 },
+  { 2104.4860839844, -1819.6783447266, 17.845899581909, 2104, -1818.8521728516, 17.560821533203 },
+  { 1503.8500976563, -1620.4155273438, 34.390701293945, 1504.2943115234, -1621.2131347656, 33.98250579834 },
+  { 1332.1098632813, -1367.7768554688, 20.461099624634, 1331.3596191406, -1368.3088378906, 20.068439483643 },
+  { 1181.0299072266, -1341.9682617188, 17.987600326538, 1181.1824951172, -1341.0618896484, 17.593641281128 },
+  { 1836.3731689453, -1318.5660400391, 20.100700378418, 1836.8768310547, -1319.3624267578, 19.765930175781 },
+  { 2234.0607910156, -1635.8319091797, 20.857799530029, 2234.1801757813, -1636.7800292969, 20.563255310059 }
+}
+
+-- **************************************************************************
+
+for i, event in ipairs(events) do
+  addEvent(event, true)
+end
+
+addEventHandler('cctv.positions.get', resourceRoot, function()
+    if not client then
+      return
+    end
+    return triggerLatentClientEvent(client, 'cctv.positions.set', 5000, false, client, positions)
+  end
+)
+
+-- **************************************************************************
+
+local function openCCTV(playerSource, cctvNumber)
+  setElementData(playerSource, 'cctv.state', true)
+  return triggerClientEvent(playerSource, 'cctv.player.set', playerSource, true, cctvNumber)
+end
+
+local function closeCCTV(playerSource, cctvNumber)
+  setElementData(playerSource, 'cctv.state', false)
+  return triggerClientEvent(playerSource, 'cctv.player.set', playerSource, false, cctvNumber)
+end
+
+local cctvActions = {
+  [false] = openCCTV,
+  [true] = closeCCTV
+}
+
+local function handleCCTV(playerSource, commandName, cctvNumber)
+  if not playerSource or getPedOccupiedVehicle(playerSource) then
+    return
+  end -- serverside validation cause we dont trust client
+  local cctvMax = #positions
+  cctvNumber = tonumber(cctvNumber or 0)
+  if cctvNumber > cctvMax then
+    return outputChatBox(string.format('KULLANIM: /%s <1 - %s>', commandName, cctvMax), playerSource, 255, 168, 0, false)
+  end
+  if getElementData(playerSource, loggedIn) and getElementData(playerSource, cctv) and not debugmode or debugmode then
+    if cctvNumber > 0 then
+      return cctvActions[false](playerSource, cctvNumber)
+    end
+    return cctvActions[(getElementData(playerSource, 'cctv.state') or false)](playerSource, cctvNumber)
+  end
+end
+
+addEventHandler('cctv.handle.cmd', resourceRoot, function()
+    if not client then
+      return
+    end
+    addCommandHandler('cctv', handleCCTV, false, false)
+  end
+)
+
+-- **************************************************************************
+
+local function removeCCTV(player)
+  return setElementData(player, 'cctv.state', false)
+end
+
+addEventHandler('onResourceStop', resourceRoot, function()
+    for i, player in ipairs(getElementsByType'player') do
+      removeCCTV(player)
+    end
+  end
+)
+
+addEventHandler('onPlayerQuit', root, function()
+    return removeCCTV(source)
+  end
+)
